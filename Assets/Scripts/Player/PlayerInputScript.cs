3eb57d1167b801aa01076ad646e2716672b3806e
m3talpillow <jaredjohans@gmail.com>
2018-11-09 15:44:08 -0500

    Locomotion Inplemented
    Basic locomotion animations added. 4 directional animations, diagonal movement is a blend of these animations.
    
    Sword can be drawn by hitting 'x' key. Animation of sword being drawn/sheathed is masked so it can play during any movement animation. Additionally, once sword is drawn the player's right hand fingers stay curled by a third animation mask. (Note: sword model is not yet implemented).

---
+++ Assets/Scripts/Player/PlayerInputScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerInputScript : MonoBehaviour
+{
+    // Dependencies
+    private Animator anim;
+
+    // Fields controlled by class
+    private float verticalInput;
+    private float horizontalInput;
+
+    private void Awake()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    void Start ()
+    {
+        anim.SetBool("Armed", false);
+	}
+	
+	void Update ()
+    {
+        GetMovementInput();
+	}
+
+    private void GetMovementInput()
+    {
+        // Get WASD/ Arrow inputs
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Set animator variables from inputs
+        if (verticalInput != 0 || horizontalInput != 0)
+            anim.SetBool("Moving", true);
+        else
+            anim.SetBool("Moving", false);
+
+
+        anim.SetFloat("Vertical", verticalInput);
+        anim.SetFloat("Horizontal", horizontalInput);
+
+        // Get Mouse inputs
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            anim.SetTrigger("MouseLeftClick");
+
+        // Get Keyboard inputs
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            bool armed = anim.GetBool("Armed");
+
+            if (armed)
+            {
+                anim.SetTrigger("SheathSword");
+                anim.ResetTrigger("DrawSword");
+            }
+            else
+            {
+                anim.SetTrigger("DrawSword");
+                anim.ResetTrigger("SheathSword");
+            }
+
+            // Toggle armed status
+            anim.SetBool("Armed", !armed);
+        }
+    }
+}
